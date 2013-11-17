dd9cc5571a31de89f07a3e762a168ec9efaa85a5
Makis Tracend <makis.tracend@gmail.com>
2013-11-17 00:06:11 -0800

    re-enabling tasks

--- lib/google.sdk.php
+++ lib/google.sdk.php
@@ -4,7 +4,7 @@
 require_once( SDK. "google/0.6.7/Google_Client.php");
 // - Include all the services you'll be using
 require_once( SDK. "google/0.6.7/contrib/Google_PlusService.php");
-//require_once( SDK. "google/0.6.7/contrib/Google_TasksService.php");
+require_once( SDK. "google/0.6.7/contrib/Google_TasksService.php");
 //require_once( SDK. "google/0.6.7/contrib/Google_ModeratorService.php");
 //require_once( SDK. "google/0.5.0/contrib/Google_CalendarService.php");
 
